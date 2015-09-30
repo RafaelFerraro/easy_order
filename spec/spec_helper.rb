7fd2fe64a73d24762aa4af107204647175376609
Rafael Ferraro <rafaferraro@hotmail.com>
2015-09-30 15:59:56 -0300

    including rspec files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,16 @@
 ENV['LOTUS_ENV'] ||= 'test'
 
 require_relative '../config/environment'
-require 'minitest/autorun'
+# require 'minitest/autorun'
 
 Lotus::Application.preload!
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
